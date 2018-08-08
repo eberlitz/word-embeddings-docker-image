61209275473446e9e96af0298762ac189827eeda
Eduardo Eidelwein Berlitz <eberlitz@gmail.com>
2018-08-08 09:42:42 -0300

    Script to train word2vec embeddings

--- README.md
+++ README.md
@@ -3,15 +3,18 @@
 # Build the image
 docker build -t word2vecf:latest .
 
-# Generate word vocabulary file
-docker run -it -v /home/berlitz/data:/usr/src/app/data word2vecf:latest python ./scripts/contexts.py ./data/ptwiki.db -o ./data/contexts/ -wv 50 -b 1000 
-
-# Generate word,context and context vocabulary files
-docker run -it -v /home/berlitz/data:/usr/src/app/data word2vecf:latest python ./scripts/contexts.py ./data/ptwiki.db -b 1000 -o ./data/contexts/ -mc 100
+# Generate word2vecf input files
+docker run -it -v /home/berlitz/data:/usr/src/app/data word2vecf:latest python ./scripts/multiprocess_contexts.py ./data/ptwiki.db -o ./data/contexts/ -b 1000
 
 # train word2vecf
 docker run -it -v /home/berlitz/data:/usr/src/app/data word2vecf:latest ./word2vecf/word2vecf -train ./data/contexts/dep.contexts -wvocab ./data/contexts/wv -cvocab ./data/contexts/cv -output ./data/dim200vecs -size 200 -negative 15 -threads 10
 
 # convert to numpy vectors
 docker run -it -v /home/berlitz/data:/usr/src/app/data word2vecf:latest python ./scripts/vecs2nps.py ./data/dim200vecs ./data/vecs
+
+# train word2vec models
+curl https://eberlitz.blob.core.windows.net/ptwiki2vec/data/ptwiki-articles-text-preprocessed.tar.gz -o ./data/ptwiki-articles-text-preprocessed
+
+docker run -it -v /home/berlitz/data:/usr/src/app/data word2vecf:latest python ./scripts/train-word2vec.py ./data/ptwiki-articles-text-preprocessed ./models/word2vec/ -size 200 -window 5 -mincount 2
+
 ```
---
+++ scripts/train-word2vec.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+import bz2
+import glob
+import gensim
+import logging
+import argparse
+import collections
+import multiprocessing
+from helpers import mkdir_if_not_exists
+
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+class MySentences(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __iter__(self):
+        filepaths = glob.glob(self.dirname)
+        filepaths.sort()
+        for fp in filepaths:
+            with bz2.BZ2File(fp, 'r') as input:
+                for line in input:
+                    yield line.decode('utf-8').split()
+
+
+def main():
+    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=__doc__)
+    parser.add_argument("input", help="ptwiki-compressed-text-folder")
+    parser.add_argument("-o", "--output", default="./models/",
+                        help="directory for extracted files")
+    parser.add_argument("-s", "--size", type=int, default=200,
+                        help="size")
+    parser.add_argument("-w", "--window", type=int, default=5,
+                        help="window")
+    parser.add_argument("-m", "--mincount", type=int, default=2,
+                        help="mincount")
+
+    args = parser.parse_args()
+    output_dirname = args.output
+    input_dir = args.input
+    size = args.size
+    window = args.window
+    min_count = args.mincount
+    output_path = args.output
+
+    mkdir_if_not_exists(output_dirname)
+
+    # '../data/ptwiki-articles-text-preprocessed
+    wiki_text_dump_path = input_dir+'/**/*.bz2'
+    sentences = MySentences(wiki_text_dump_path)
+
+    mkdir_if_not_exists(output_path)
+
+    # build vocabulary and train model
+    model = gensim.models.Word2Vec(sentences,
+                                   size=size,
+                                   window=window,
+                                   min_count=min_count,
+                                   workers=multiprocessing.cpu_count())
+
+    # model.train(documents, total_examples=len(documents), epochs=10)
+
+    # trim unneeded model memory = use (much) less RAM
+    # Precompute L2-normalized vectors.
+    # If replace is set, forget the original vectors and only keep the normalized ones = saves lots of memory!
+    # Note that you cannot continue training after doing a replace. The model becomes effectively read-only = you can call most_similar, similarity etc., but not train.
+    model.init_sims(replace=True)
+
+    model_file_name = os.path.join(
+        output_path, 'word2vec-s{0}-w{1}-m{2}'.format(size, window, min_count))
+    # model.save(model_file_name)
+    model.wv.save_word2vec_format(
+        '{0}.bin'.format(model_file_name), binary=True)
+
+    word_vectors = model.wv
+    # word_vectors.save(output_path)
+
+    print(word_vectors.most_similar(positive=['carro'], topn=10))
+    print("Most similar to {0}".format(
+        word_vectors.most_similar(positive="america")))
+    print('CONCLUDED')
+
+
+if __name__ == '__main__':
+    main()
